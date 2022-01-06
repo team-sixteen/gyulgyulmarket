1d00fa170375997e3adbeaacfcb9449f4308e792
Danim Kim <kdn725@gmail.com>
2022-01-06 09:05:30 +0900

    [feat] 검색하기 버튼 눌렀을때 검색창 보이기

--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,7 @@ const btnSearch = document.querySelector('.m-btn');
 const mainNav = document.querySelector('.main-nav');
 const searchNav = document.querySelector('.search-nav');
 const navBtnSearch = document.querySelector('.nav-top__btn--search');
-const btnBack = document.querySelector('');
+// const btnBack = document.querySelector('');
 
 function showSearchBar(){
     mainNav.classList.toggle('hide');

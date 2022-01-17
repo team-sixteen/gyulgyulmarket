dcc7f24e2dbb608ab2358dad19464645bcc9a78b
BigHuni <hdh8659@gmail.com>
2022-01-17 10:34:28 +0900

    [add] modal js 추가

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,27 +1,38 @@
-// 특정 버튼 누르면 모달창 켜기
-const modal = document.getElementById(".post-modal__wrapper")
-const btnModal = document.getElementById(".btn-post-menu")
-btnModal.addEventListener("click", e => {
-    modal.style.display = "flex"
-})
+const open = document.getElementById(".btn-post-menu")
+const modal = document.querySelector(".post-modal-hidden")
 
-// 모달창 클로즈 버튼 누르면 모달창 닫기
-const closeBtn = modal.querySelector(".close-area")
-closeBtn.addEventListener("click", e => {
-    modal.style.display = "none"
-})
+open.onclick = () => {
+    modal.style.display = "flex";
+};
 
-// 모달창 바깥 영역 클릭 시 모달창 닫기
-modal.addEventListener("click", e => {
-    const evTarget = e.target
-    if(evTarget.classList.contains("modal-overlay")) {
-        modal.style.display = "none"
-    }
-})
 
-// 모달창 켜진 상태에서 Esc 클릭 시 모달창 닫기
-window.addEventListener("keyup", e => {
-    if(modal.style.display === "flex" && e.key === "Escape") {
-        modal.style.display = "none"
-    }
-})
+
+
+
+// // 특정 버튼 누르면 모달창 켜기
+// const modal = document.getElementById(".post-modal__wrapper")
+// const btnModal = document.getElementById(".btn-post-menu")
+// btnModal.addEventListener("click", e => {
+//     modal.style.display = "flex"
+// })
+
+// // 모달창 클로즈 버튼 누르면 모달창 닫기
+// const closeBtn = modal.querySelector(".close-area")
+// closeBtn.addEventListener("click", e => {
+//     modal.style.display = "none"
+// })
+
+// // 모달창 바깥 영역 클릭 시 모달창 닫기
+// modal.addEventListener("click", e => {
+//     const evTarget = e.target
+//     if(evTarget.classList.contains("modal-overlay")) {
+//         modal.style.display = "none"
+//     }
+// })
+
+// // 모달창 켜진 상태에서 Esc 클릭 시 모달창 닫기
+// window.addEventListener("keyup", e => {
+//     if(modal.style.display === "flex" && e.key === "Escape") {
+//         modal.style.display = "none"
+//     }
+// })
